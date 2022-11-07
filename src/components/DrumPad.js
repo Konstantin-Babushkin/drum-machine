7a2cc5df06f3c36c6f82395f8299ec0b479b48dd
Konstantin-Babushkin <constant-babushkin@yandex.ru>
2022-11-07 19:25:22 +0300

    changed the whole app. Added custom sounds

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,66 +1,58 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 
-export default class DrumPad extends Component {
+const pressed = {
+  backgroundColor: "#fffa57",
+};
+
+const unpressed = {
+  backgroundColor: "#eff0f2",
+};
+export default class DrumPad extends React.Component {
   constructor(props) {
     super(props);
-
-    this.audioHandler = React.createRef();
-    this.onDrumPadClicked = this.onDrumPadClicked.bind(this);
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onKeyUp = this.onKeyUp.bind(this);
-  }
-
-  onDrumPadClicked() {
-    const text = this.props.padItem.id.replace(/-/g, " ");
-    const audioElm = this.audioHandler.current;
-
-    this.props.updateDisplayText(text);
-    audioElm.currentTime = 0;
-    audioElm.play();
-  }
-
-  onKeyDown(event) {
-    const root = ReactDOM.findDOMNode(this);
-
-    if (event.keyCode === this.props.padItem.keyCode) {
-      root.classList.add("active");
-      this.onDrumPadClicked();
-    }
-  }
-
-  onKeyUp(event) {
-    const root = ReactDOM.findDOMNode(this);
-
-    if (event.keyCode === this.props.padItem.keyCode) {
-      setTimeout(() => {
-        root.classList.remove("active");
-      }, 33);
-    }
-  }
-
-  componentDidUpdate() {
-    this.audioHandler.current.volume = this.props.volumeValue / 100;
+    this.state = {
+      style: unpressed,
+    };
+    this.playSound = this.playSound.bind(this);
+    this.handleKeydown = this.handleKeydown.bind(this);
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.onKeyDown);
-    document.addEventListener('keyup', this.onKeyUp);
+    document.addEventListener("keydown", this.handleKeydown);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.onKeyDown);
-    document.removeEventListener('keyup', this.onKeyUp);
+    document.removeEventListener("keydown", this.handleKeydown);
+  }
+
+  handleKeydown(e) {
+    if (e.keyCode === this.props.data.keyCode) {
+      this.playSound();
+    }
+  }
+
+  playSound(e) {
+    const { id, keyTrigger } = this.props.data;
+    const sound = document.getElementById(keyTrigger);
+    sound.currentTime = 0;
+    sound.play();
+    this.props.onPlay(id);
+    this.setState({ style: pressed });
+    setTimeout(() => this.setState({ style: unpressed }), 100);
   }
 
   render() {
-    const padItem = this.props.padItem;
-
+    const { id, url, keyTrigger } = this.props.data;
     return (
-      <div className="drum-pad" id={padItem.id} onClick={this.onDrumPadClicked}>
-        <audio className="clip" id={padItem.keyTrigger} src={padItem.url} ref={this.audioHandler} />
-        {padItem.keyTrigger}
+      <div
+        className="drum-pad"
+        id={id}
+        onClick={this.playSound}
+        style={this.state.style}
+      >
+        <audio id={keyTrigger} src={url} className="clip"></audio>
+        {keyTrigger}
       </div>
-    )
+    );
   }
 }
